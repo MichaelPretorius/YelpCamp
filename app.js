8a988000d04912ea82b21cf9509a3b1d42552f85
Michael Pretorius <michaelpretorius96@gmail.com>
2019-05-26 15:01:47 +0000

    update db url

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ var express         = require("express"),
 var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
-
-mongoose.connect("mongodb+srv://Michael:Mcp9602065144085@michael-gbapw.mongodb.net/YelpCamp?retryWrites=true", {useNewUrlParser: true});
+    
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+// mongoose.connect("mongodb+srv://Michael:Mcp9602065144085@michael-gbapw.mongodb.net/YelpCamp?retryWrites=true", {useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
